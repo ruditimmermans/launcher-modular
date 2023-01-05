35d3695320a10d73620103ddde32e62b1b074778
Rudi Timmermans <rudi.timmermans@mailfence.com>
2023-01-05 17:50:45 +0100

    Update Dutch translation

--- build/aarch64-linux-gnu/app/po/CMakeFiles/launchermodular.ubuntouchfr.pot.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/launchermodular.ubuntouchfr.pot"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/launchermodular.ubuntouchfr.pot.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
